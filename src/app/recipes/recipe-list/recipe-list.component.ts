2f41f61c0a68a5280787a15c87c3e85239c80528
mahmoud-hany <mahmoud.hany.5220@gmail.com>
2018-10-03 15:28:07 +0200

    Create Project Main components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeModel } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: RecipeModel[] = [
+    new RecipeModel(`Frying pan pizza`, 
+      `Weigh the ingredients for the dough into a large bowl and add 1/2 tsp 
+      salt and 125ml warm water. Mix to form a soft dough, then tip 
+      onto your work surface ` , 
+      `https://www.bbcgoodfood.com/sites/default/files/recipe/recipe-image/2016/09/frying-pan-pizza.jpg`),
+      
+    new RecipeModel(`Frying pan pizza`, 
+      `Weigh the ingredients for the dough into a large bowl and add 1/2 tsp 
+      salt and 125ml warm water. Mix to form a soft dough, then tip 
+      onto your work surface ` , 
+      `https://www.bbcgoodfood.com/sites/default/files/recipe/recipe-image/2016/09/frying-pan-pizza.jpg`)
+  ];
 
   constructor() { }
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+// export class Ingrediant {
+//     public name: string;
+//     public amount: number;
+
+//     constructor(name: string, amount: number) {
+//         this.name = name;
+//         this.amount = amount;
+//     }
+// }
+
+// This is alias [Shortcut] to the above 
+export class Ingredient {
+    constructor(public name: string, public amount: number){}
+}
